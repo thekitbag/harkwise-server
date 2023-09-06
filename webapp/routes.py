3ef69c1514a7e00f158842fdd59b0929969a7562
Mark Gray <mfgray87@gmail.com>
2023-09-06 20:19:14 +0100

    taking info from request and writing to DB

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,5 +1,20 @@
-from webapp import app
+from flask import json, request
+
+from webapp import app, db
+from webapp.models import Establishment, Rating
+
 
 @app.route('/api/review', methods=["POST"])
 def review():
+    data = json.loads(request.data.decode('utf-8'))
+    establishment_name = data['establishmentName']
+    rating = data['rating']
+    comment = data['comment']
+
+    e = Establishment(name=establishment_name)
+    r = Rating(rating=rating, comment=comment)
+    e.ratings.append(r)
+    db.session.add_all([e,r])
+    db.session.commit()
+
     return "Thanks for leaving a review"
