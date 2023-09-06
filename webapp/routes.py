83d8ca300c4ee272562b711769221db961efcf64
Mark Gray <mfgray87@gmail.com>
2023-09-06 21:08:11 +0100

    API for getting the establishment name

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,20 +1,29 @@
-from flask import json, request
+from flask import json, request, jsonify
 
 from webapp import app, db
 from webapp.models import Establishment, Rating
+
+@app.route('/api/get_establishment_details/<int:establishment_id>', methods=['GET'])
+def get_establishment_details(establishment_id):
+    establishment = Establishment.query.get(establishment_id)
+    if establishment:
+        name = establishment.name
+        return jsonify({'name': name})
+    else:
+        return jsonify({'error': 'Establishment not found'}), 404
 
 
 @app.route('/api/review', methods=["POST"])
 def review():
     data = json.loads(request.data.decode('utf-8'))
-    establishment_name = data['establishmentName']
+    establishment_id = data['establishmentId']
     rating = data['rating']
     comment = data['comment']
 
-    e = Establishment(name=establishment_name)
+    e = Establishment.query.get(establishment_id)
     r = Rating(rating=rating, comment=comment)
     e.ratings.append(r)
     db.session.add_all([e,r])
     db.session.commit()
 
-    return "Thanks for leaving a review"
+    return "Review Received"
