8553a0136bb3568ff3f5e270e26d8aa585d53ada
Mark Gray <mfgray87@gmail.com>
2023-09-12 20:25:27 +0100

    updated requirements

---
+++ boot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script is used to boot a Docker container
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+
+exec gunicorn -b :5000 --access-logfile - --error-logfile - runserver:app
